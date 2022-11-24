26766ce3be6a6ada1c9cbbbf577eaf546d58db9c
developeruche <nweleuchenna2004@gmail.com>
2022-11-24 22:49:11 +0100

    developeruche: done with the project init and starter working on to onboarding facet

---
+++ contracts/facets/OnBoardingFacet.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+/// @notice this contract would be used to unboard new protocol in the application
+contract OnBoardFacet {
+
+    /// @notice this function would be used to change the address of the a protocol
+    /// @dev this function would be guided with access control and this function would have the power to change protocol address in other depending contract
+    /// @param _addr: this the new address of the protocol 
+    function change_protocol_address(address _addr) external {
+
+    }
+}
