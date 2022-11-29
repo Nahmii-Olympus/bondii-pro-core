84e59efb4c4159fe23c6f1684c38d636e9703ecb
Abiodun <aabiodunawoyemi@gmail.com>
2022-11-29 14:03:44 +0100

    abiodun: onboarding facet

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -10,10 +10,13 @@ pragma solidity ^0.8.0;
 
 import { LibDiamond } from "./libraries/LibDiamond.sol";
 import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
+import { OnBoarding } from "./libraries/LibAppStorage.sol";
 
 contract Diamond {
+
+    OnBoarding internal ob;
     
-    constructor(address _contractOwner, address _diamondCutFacet) payable {        
+    constructor(address _contractOwner, address _diamondCutFacet, address _bondiiTreasury, address _bondiiProFactoryStorage, address _bondiiProSubsidyRouter,address _stakingFactory, address _bondiiDAO) payable {        
         LibDiamond.setContractOwner(_contractOwner);
 
         // Add the diamondCut external function from the diamondCutFacet
@@ -25,7 +28,19 @@ contract Diamond {
             action: IDiamondCut.FacetCutAction.Add, 
             functionSelectors: functionSelectors
         });
-        LibDiamond.diamondCut(cut, address(0), "");     
+        LibDiamond.diamondCut(cut, address(0), "");
+
+
+        require( _bondiiTreasury != address(0) );
+        ob.bondiiTreasury = _bondiiTreasury;
+        require( _bondiiProFactoryStorage != address(0) );
+        ob.bondiiProFactoryStorage = _bondiiProFactoryStorage;
+        require( _bondiiProSubsidyRouter != address(0) );
+        ob.bondiiProSubsidyRouter = _bondiiProSubsidyRouter;
+        require( _stakingFactory != address(0) );
+        ob.stakingFactory = _stakingFactory; 
+        require( _bondiiDAO != address(0) );
+        ob.bondiiDA0 = _bondiiDAO; 
     }
 
     // Find facet for function that is called and execute the
--- contracts/facets/OnBoardingFacet.sol
+++ contracts/facets/OnBoardingFacet.sol
@@ -1,14 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {BondiiProBond} from "../modules/Bond.sol";
+import {StakingRewards} from "../modules/staking.sol";
+import {Treasury} from "../modules/Treasury.sol";
+import { OnBoarding } from "../libraries/LibAppStorage.sol";
 
-/// @notice this contract would be used to unboard new protocol in the application
+/// @notice this contract would be used to onboard new protocol in the application
 contract OnBoardFacet {
+    OnBoarding internal ob;
+
+    event TreasuryDeployed(address treasury_addr, address _bondPayoutToken, address _stakingPayoutToken, uint256 time);
+    event StakingDeployed(address staking_addr, address owner, address _rewardsDistribution, address _rewardsToken, uint time);
+    event BondDeployed(address bond_addr, address treasury_addr, uint256 time);
 
     /// @notice this function would be used to change the address of the a protocol
     /// @dev this function would be guided with access control and this function would have the power to change protocol address in other depending contract
     /// @param _addr: this the new address of the protocol 
     function change_protocol_address(address _addr) external {
+        onlyDAO;
+        ob.bondiiTreasury = _addr;
+    }
 
+    /// @dev this function will be access control to change protocol address in other depending contract
+    function onlyDAO() internal view {
+        require(ob.bondiiDA0 == msg.sender, "caller is not the DAO" );
+    }
+
+    /// @notice this function would be used to create bond, treasury and staking of the a protocol
+    /// @dev this function would be guided with access control and this function would have the power to change protocol address in other depending contract
+    /// @param _bondPayoutToken: this the address of the bond payout token 
+    /// @param _stakingPayoutToken: this the address of the staking payout token 
+    /// @param _rewardsDistribution: this the address of the rewards distribution 
+    /// @param _rewardsToken: this the address of the rewards token 
+    /// @param _rewardsDuration: this the time frame of the rewards 
+    function createBondTreasuryStaking(
+        address _bondPayoutToken,
+        address _stakingPayoutToken, // i used this in treasury and staking
+        address _rewardsDistribution,
+        address _rewardsToken,
+        uint256 _rewardsDuration
+    ) external {
+        onlyDAO;
+
+        Treasury _treasury = new Treasury(_bondPayoutToken, _stakingPayoutToken);
+
+        StakingRewards _staking = new StakingRewards(msg.sender, _rewardsDistribution, _rewardsToken, _stakingPayoutToken, _rewardsDuration);
+
+        BondiiProBond _bond = new BondiiProBond(address(_treasury), msg.sender);
+        
+        // address stakingContract = IStakingFactory(stakingFactory).createNewStake(_payoutToken);
+
+        emit TreasuryDeployed(address(_treasury), _bondPayoutToken, _stakingPayoutToken, block.timestamp);
+        emit StakingDeployed(address(_staking), msg.sender, address(_rewardsDistribution), address(_rewardsToken), block.timestamp);
+        emit BondDeployed(address(_bond), address(_treasury), block.timestamp);
     }
 }
